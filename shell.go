139b4a9aa15fe588ea86fe08a552a29b0a211704
Rana Umar Nadeem <ranaumarnadeem632@gmail.com>
2025-04-22 13:29:38 +0500

    Update help command to include additional built-in commands and their descriptions

--- shell.go
+++ shell.go
@@ -17,12 +17,17 @@ func help() {
 	fmt.Println("Potato Shell - Built-in Commands:")
 	fmt.Println("______________________________________________________________________________")
 	fmt.Println("|  ls [-l] [-a] [dir]  |   List files in the current or specified directory")
-	fmt.Println("|      -l              |  Use a long listing format")
+	fmt.Println("|      -l              |   Use a long listing format")
 	fmt.Println("|      -a              |   Show hidden files")
 	fmt.Println("|  cd <dir>            |   Change the current directory")
 	fmt.Println("|  open <file>         |   Open a file with the default associated program")
 	fmt.Println("| help                 |   Show this help message")
 	fmt.Println("|  exit                |   Exit the shell")
+	fmt.Println("|  echo				|	Does whatever echo do")
+	fmt.Println("|  history             |   Show past commands")
+	fmt.Println("|  alias name command  |   Create a shortcut")
+	fmt.Println("|  unalias name        |   Remove a shortcut")
+	fmt.Println("|  which				|	Search for file or command in system path")
 	fmt.Println("______________________________________________________________________________")
 }
 
